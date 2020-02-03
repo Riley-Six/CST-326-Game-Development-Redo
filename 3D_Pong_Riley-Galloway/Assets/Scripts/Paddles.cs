731ee665c56df80cb418430a97f4156403ece1d8
Riley-Six <60455700+Riley-Six@users.noreply.github.com>
2020-02-03 15:10:33 -0800

    Fixed Stuff

---
+++ 3D_Pong_Riley-Galloway/Assets/Scripts/Paddles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool isLpaddle;
+    //Left = True, Right = False
+    public float speed = 4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLpaddle)
+        {
+            transform.Translate(0f, Input.GetAxis("Horizontal1") * speed * Time.deltaTime, 0f);
+        }
+        else
+        {
+            transform.Translate(0f, Input.GetAxis("Horizontal2") * speed * Time.deltaTime, 0f);
+        }
+
+    }
+}
