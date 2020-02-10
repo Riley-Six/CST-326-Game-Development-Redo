c824c23f77927f7bfca7a0474dd455f8b52c8a7e
Riley-Six <60455700+Riley-Six@users.noreply.github.com>
2020-02-09 23:47:30 -0800

    New Version

---
+++ Prof Pong 2.0/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public AudioClip hitSound;
+
+    public AudioSource speaker;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        { 
+        speaker.PlayOneShot(hitSound);
+        
+        }
+    
+    }
+}
---
+++ Prof Pong 2.0/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    [SerializeField] private Text leftTextScore;
+    [SerializeField] private Text rightTextScore;
+
+    [SerializeField] private Goal leftGoal;
+    [SerializeField] private Goal rightGoal;
+
+    [SerializeField] private GameManager gameManager;
+
+
+    private int leftScore = 0;
+
+    private int rightScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshScore();
+    }
+
+    private void RefreshScore()
+    {
+        //some function to update the Text string
+        leftTextScore.text = "" + leftScore + "";
+        rightTextScore.text = "" + rightScore + "";
+        leftTextScore.fontSize = 1 + leftScore/4 ;
+        rightTextScore.fontSize = 1 + rightScore/4 ;
+
+
+    }
+    public void AddScore(Goal scoringSide)
+    {
+        if (scoringSide == leftGoal)
+        {
+            rightScore += 1;
+        }
+
+        else
+        {
+            leftScore += 1;
+        }
+        RefreshScore();
+    }
+}
