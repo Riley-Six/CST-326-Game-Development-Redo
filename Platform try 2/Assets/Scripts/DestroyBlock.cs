507ea442a8619523d2e739142c7d5fd0f67203af
Riley-Six <60455700+Riley-Six@users.noreply.github.com>
2020-02-16 23:30:15 -0800

    Platformer added
    Platformer

---
+++ Platform try 2/Assets/Scripts/DestroyBlock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBlock : MonoBehaviour
+{
+
+
+    public GameObject Block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Block.GetComponent<GameObject>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                BoxCollider bc = hit.collider as BoxCollider;
+
+                if (bc != null)
+                {
+                    Destroy(bc.gameObject);
+                }
+            }
+        }
+    }
+
+    
+
+}
---
+++ Platform try 2/Assets/Scripts/TimeCount.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TimeCount : MonoBehaviour
+{
+    
+    float timeleft = 375.0f;
+    public TextMeshProUGUI text;
+
+
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+        text.SetText(Mathf.Round(timeleft) + "");
+        
+
+        if (timeleft < 0)
+        {
+            SceneManager.LoadScene("LevelParser");
+        }
+    }
+
+    public void ResetTimer()
+    {
+        timeleft = 375.0f;
+    }
+}
