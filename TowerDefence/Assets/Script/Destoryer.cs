5c2261d2effb9290a2267acb25e1d67d8731b58d
Riley-Six <60455700+Riley-Six@users.noreply.github.com>
2020-03-22 21:36:35 -0700

    Tower Defense

---
+++ TowerDefence/Assets/Script/Destoryer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Destoryer : MonoBehaviour
+{
+
+    public GameObject bad1;
+
+    public GameObject bad2;
+
+    public int points = 0;
+    [SerializeField] public Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bad1.GetComponent<GameObject>();
+        bad2.GetComponent<GameObject>();
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                BoxCollider bc = hit.collider as BoxCollider;
+
+                if (bc != null)
+                {
+                    Destroy(bc.gameObject);
+                    points += 10;
+                    score.text = "" + points + "";
+                }
+            }
+        }
+    }
+}
---
+++ TowerDefence/Assets/Script/WaypointManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+  public Waypoint[] waypoint;
+
+  void Awake()
+  {
+
+    waypoint = GetComponentsInChildren<Waypoint>();
+    
+  }
+
+  public Waypoint GetNeWaypoint(int currentIndex)
+  {
+    return waypoint[currentIndex++];
+  }
+}
